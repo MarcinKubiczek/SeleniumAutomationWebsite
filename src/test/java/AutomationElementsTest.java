3a09972b89f019f6a3bba8516f649a35098fe825
Marcin Kubiczek <Kubiczek.M@eplan.pl>
2021-09-10 14:41:06 +0200

    ADD:New Test Case

--- src/test/java/AutomationElementsTest.java
+++ src/test/java/AutomationElementsTest.java
@@ -1,54 +1,88 @@
 import io.qameta.allure.Allure;
+import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
 
 public class AutomationElementsTest {
 
     private WebDriver driver;
+    private WebDriverWait wait;
+    final private String seleniumAutomationWebsite = "https://rahulshettyacademy.com/seleniumPractise/";
+    final private String automationPracticeWebsite = "https://rahulshettyacademy.com/AutomationPractice/";
 
     @BeforeAll
     public static void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\kubiczem\\IdeaProjects\\SeleniumAutomationWebsite\\src\\main\\resources\\chromedriver.exe");
+
     }
 
     @BeforeEach
     public void setUpBeforeEach() {
         this.driver = new ChromeDriver();
+        this.wait = new WebDriverWait(driver, 20);
 
     }
 
     @AfterEach
     public void cleanUpAfterEach() {
         Allure.getLifecycle().addAttachment(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMM-yy_hh:mm:ss")), "image/png", "png", makeScreenShot());
-        driver.close();
+        driver.quit();
+        ;
     }
+
     private byte[] makeScreenShot() {
         return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
+
     @Test
-    public void clickOnCalendarTest(){
+    public void clickOnCalendarTest() {
 
     }
+
     @Test
     public void testCheckboxes() {
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.get(automationPracticeWebsite);
         WebElement checkbox1 = driver.findElement(By.xpath("//input[@id='checkBoxOption1']"));
         Assertions.assertFalse(checkbox1.isSelected());
         checkbox1.click();
         Assertions.assertTrue(checkbox1.isSelected());
-        driver.quit();
     }
 
     @Test
     public void countTestCheckboxes() {
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.get(automationPracticeWebsite);
         List<WebElement> boxes = driver.findElements(By.xpath("//div[@id='checkbox-example']//input[contains(@id,'checkBoxOption')]"));
         System.out.println(boxes.size());
+    }
+
+    @Test
+    public void addElementsWithSimilarLocators() throws InterruptedException {
+        driver.get(seleniumAutomationWebsite);
+        String[] itemToAdd = {"Cucumber", "Tomato"};
+        By products= By.cssSelector("h4.product-name");
+        By numberOfElementInBracket = By.xpath("//tr[1]/td[3]/strong");
+        wait.until((ExpectedConditions.presenceOfElementLocated(products)));
+        List<WebElement> allProducts = driver.findElements(products);
+        for (int i = 0; i < allProducts.size(); i++) {
+            String[] productName = allProducts.get(i).getText().split("-");
+            String formattedName = productName[0].trim();
+
+            List itemsNeededList = Arrays.asList(itemToAdd);
+            if (itemsNeededList.contains(formattedName)) {
+                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+            }
+        }
+        int itemsInBracket = Integer.parseInt(driver.findElement(numberOfElementInBracket).getText());
+        int expectedItemsInBracket=2;
+        Assert.assertEquals(itemsInBracket,expectedItemsInBracket);
     }
 
 }
